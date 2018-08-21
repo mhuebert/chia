8ffdf40d50a6915f6614b63253b4ba406fd53f1e
Matthew Huebert <mhuebert@gmail.com>
2018-08-21 17:58:34 +0200

    defer to tachyons

--- jss/src/chia/jss/functional_styles.cljs
+++ jss/src/chia/jss/functional_styles.cljs
@@ -24,49 +24,38 @@
                 :or {unit 8
                      break-small 420}}]]
   {"@global"
-   (merge {:.flex {:display "flex"}
-           :.flex-auto {:flex "1 1 auto"
-                        :min-width 0
-                        :min-height 0}
-           :.flex-none {:flex "none"}
-           :.justify-center {:justify-content "center"}
-           :.flex-wrap {:flex-wrap "wrap"}
-           :.items-center {:align-items "center"}
-           :.items-stretch {:align-items "stretch"}
-           :.center {:margin-left "auto"
-                     :margin-right "auto"}
-           :.text-center {:text-align "center"}
-           :.hover-opacity-1 {"&:hover" {:opacity 1}}
+   (merge {:.hover-opacity-1 {"&:hover" {:opacity 1}}
            :.hover-opacity-parent {:cursor "pointer"
                                    "& .hover-opacity-child" {:opacity 0.5}
                                    "&:hover .hover-opacity-child" {:opacity 1}}
-           :.overflow-hidden {:overflow "hidden"}
+           #_#_:.overflow-hidden {:overflow "hidden"}
            :.b-solid {:border-style "solid"}
            :.b-none {:border "none"}
            :.no-underline {:text-decoration "none"}
            :.display-link {:text-decoration "none"
                            :cursor "pointer"
                            "&:hover" {:text-decoration "underline"}}}
+
           (u/for-map [side [:top :left :right :bottom]]
-            {(str ".b-" (name side)) {(str "border-" (name side) "-style") "solid"}
-             (str "." (name side) "-0") {side 0}})
-          (u/for-map [[i v] (->> [unit
-                                  (* 1.5 unit)
-                                  (* 2 unit)
-                                  (* 2.5 unit)
-                                  (* 3 unit)
-                                  (* 4 unit)]
-                                 (interleave (range 1 999))
-                                 (partition 2))]
-            {(str ".height-" i) {:height v}
-             (str ".width-" i) {:width v}})
+              {(str ".b-" (name side)) {(str "border-" (name side) "-style") "solid"}
+               (str "." (name side) "-0") {side 0}})
+          #_(u/for-map [[i v] (->> [unit
+                                    (* 1.5 unit)
+                                    (* 2 unit)
+                                    (* 2.5 unit)
+                                    (* 3 unit)
+                                    (* 4 unit)]
+                                   (interleave (range 1 999))
+                                   (partition 2))]
+              {(str ".height-" i) {:height v}
+               (str ".width-" i) {:width v}})
           {:.width-100p {:width "100%"}}
           (u/for-map [n (range 17)]
             {(str ".round-" n) {:border-radius n}
              (str ".b-" n) {:border-width n}})
-          (u/for-map [x ["pointer"
-                         "default"]]
-            {(str ".cursor-" x) {:cursor x}})
+          #_(u/for-map [x ["pointer"
+                           "default"]]
+              {(str ".cursor-" x) {:cursor x}})
           (u/for-map [n (range 0 11)]
             {(str ".opacity-" n) {:opacity (* n 0.1)}})
           (u/for-map [[n v] (->> (interleave (range 1 9)
@@ -88,52 +77,65 @@
                       :let [color-name (name n)]]
             {(str "." color-name) {:color v}
              (str ".bg-" color-name) {:background-color v}})
-          (u/for-map [i (range 11)]
-            {(str ".opacity-" (* i 10)) {:opacity (/ i 10)}})
+          #_(u/for-map [i (range 11)]
+              {(str ".opacity-" (* i 10)) {:opacity (/ i 10)}})
 
 
           (with-breakpoint break-small
+            {#_#_:.flex {:display "flex"}
+             #_#_:.flex-auto {:flex "1 1 auto"
+                              :min-width 0
+                              :min-height 0}
+             #_#_:.flex-none {:flex "none"}
+             #_#_:.justify-center {:justify-content "center"}
+             #_#_:.flex-wrap {:flex-wrap "wrap"}
+             #_#_:.items-center {:align-items "center"}
+             #_#_:.items-stretch {:align-items "stretch"}
+             #_#_:.items-end {:align-items "flex-end"}
+             #_#_:.center {:margin-left "auto"
+                           :margin-right "auto"}
+             :.text-center {:text-align "center"}}
             (u/for-map [[rule values] {:display ["flex"
-                                                 "block"
-                                                 "inline-block"
-                                                 "inline-flex"]
-                                       :position ["relative"
-                                                  "absolute"
-                                                  "fixed"]}
-                        value values]
-              {(str "." value) {rule value}})
-            (u/for-map [direction ["row"
-                                   "column"]]
-              {(str ".flex-" direction) {:flex-direction direction}})
-            (u/for-map [n (conj (range 0 11)
-                                0.5 1.5 2.5)]
-              {(str ".margin-" (num->str n)) {:margin (* unit n)}
-               (str ".margin-h-" (num->str n)) {:margin-left (* unit n)
-                                                :margin-right (* unit n)}
-               (str ".margin-v-" (num->str n)) {:margin-top (* unit n)
-                                                :margin-bottom (* unit n)}
-               (str ".margin-t-" (num->str n)) {:margin-top (* unit n)}
-               (str ".margin-b-" (num->str n)) {:margin-bottom (* unit n)}
-               (str ".margin-r-" (num->str n)) {:margin-right (* unit n)}
-               (str ".margin-l-" (num->str n)) {:margin-left (* unit n)}
+                                                   "block"
+                                                   "inline-block"
+                                                   "inline-flex"]
+                                         :position ["relative"
+                                                    "absolute"
+                                                    "fixed"]}
+                          value values]
+                {(str "." value) {rule value}})
+            #_(u/for-map [direction ["row"
+                                     "column"]]
+                {(str ".flex-" direction) {:flex-direction direction}})
+            #_(u/for-map [n (conj (range 0 11)
+                                  0.5 1.5 2.5)]
+                {(str ".margin-" (num->str n)) {:margin (* unit n)}
+                 (str ".margin-h-" (num->str n)) {:margin-left (* unit n)
+                                                  :margin-right (* unit n)}
+                 (str ".margin-v-" (num->str n)) {:margin-top (* unit n)
+                                                  :margin-bottom (* unit n)}
+                 (str ".margin-t-" (num->str n)) {:margin-top (* unit n)}
+                 (str ".margin-b-" (num->str n)) {:margin-bottom (* unit n)}
+                 (str ".margin-r-" (num->str n)) {:margin-right (* unit n)}
+                 (str ".margin-l-" (num->str n)) {:margin-left (* unit n)}
 
-               (str ".margin-t-n" (num->str n)) {:margin-top (* unit (- n))}
-               (str ".margin-b-n" (num->str n)) {:margin-bottom (* unit (- n))}
-               (str ".margin-r-n" (num->str n)) {:margin-right (* unit (- n))}
-               (str ".margin-l-n" (num->str n)) {:margin-left (* unit (- n))}
-               (str ".margin-h-n" (num->str n)) {:margin-left (* unit (- n))
-                                                 :margin-right (* unit (- n))}
-               (str ".margin-v-n" (num->str n)) {:margin-top (* unit (- n))
-                                                 :margin-bottom (* unit (- n))}
+                 (str ".margin-t-n" (num->str n)) {:margin-top (* unit (- n))}
+                 (str ".margin-b-n" (num->str n)) {:margin-bottom (* unit (- n))}
+                 (str ".margin-r-n" (num->str n)) {:margin-right (* unit (- n))}
+                 (str ".margin-l-n" (num->str n)) {:margin-left (* unit (- n))}
+                 (str ".margin-h-n" (num->str n)) {:margin-left (* unit (- n))
+                                                   :margin-right (* unit (- n))}
+                 (str ".margin-v-n" (num->str n)) {:margin-top (* unit (- n))
+                                                   :margin-bottom (* unit (- n))}
 
-               (str ".pad-" (num->str n)) {:padding (* unit n)}
-               (str ".pad-h-" (num->str n)) {:padding-left (* unit n)
-                                             :padding-right (* unit n)}
-               (str ".pad-v-" (num->str n)) {:padding-top (* unit n)
-                                             :padding-bottom (* unit n)}
-               (str ".pad-t-" (num->str n)) {:padding-top (* unit n)}
-               (str ".pad-b-" (num->str n)) {:padding-bottom (* unit n)}
-               })
+                 (str ".pad-" (num->str n)) {:padding (* unit n)}
+                 (str ".pad-h-" (num->str n)) {:padding-left (* unit n)
+                                               :padding-right (* unit n)}
+                 (str ".pad-v-" (num->str n)) {:padding-top (* unit n)
+                                               :padding-bottom (* unit n)}
+                 (str ".pad-t-" (num->str n)) {:padding-top (* unit n)}
+                 (str ".pad-b-" (num->str n)) {:padding-bottom (* unit n)}
+                 })
             (for [px (range 8 30)]
               [(str ".text-size-" px) {:font-size (str px "px")}])
             (for [n [18 24 36 48]]
